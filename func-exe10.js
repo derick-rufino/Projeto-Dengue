d0c8dbf6cb79184bdff44650db6c4c7f12d71b62
derick-rufino <derick10011@gmail.com>
2025-03-27 15:45:10 -0300

    Adição de conteúdo e mudanças no layout

--- func-exe10.js
+++ func-exe10.js
@@ -1,23 +1,31 @@
 //váriaveis globais
-const description = document.getElementById("desc-p")
+const descSection = document.getElementById("desc-s")
 const imgSection = document.getElementById("img-holder")
+const descTitle = document.getElementById("d-title")
 
+
+//LEMBRETE: pesquisar sobre parâmetros em funções js - evitar redundancias como essa abaixo
 function addSin(){
-    description.innerHTML = "1"
+    descTitle.innerHTML = "Quais são os sintomas da dengue?"
 }
 
 function addTrans(){
-    description.innerHTML = "2"
+    descTitle.innerHTML = "Formas de Transmissão"
 }
 
 function addDiag(){
-    description.innerHTML = "3"
+    descTitle.innerHTML = "Diagnóstico"
 }
 
 function addTrat(){
-    description.innerHTML = "4"
+    descTitle.innerHTML = "Tratamento"
 }
 
 function addPrev(){
-    description.innerHTML = "5"
+    descTitle.innerHTML = "Como previnir a dengue?";
+
+    //adicionando a lista
+    descSection.innerHTML = "<ul><li>Mantenha a caixa d´água fechada </li><li>mantenha tampados tonéis e barris d´água</li><li>lave semanalmente com escova e sabão os tanques utilizados para armazenar água </li><li>encha de areia até a borda os pratos das plantas</li><li>coloque no lixo todo objeto não utilizado que possa acumular água </li><li>coloque o lixo em sacos plásticos e mantenha a lixeira bem fechada </li><li>mantenha as calhas limpas</li><li>não deixe água acumulada sobre a laje</li></ul>";
+    imgSection.style.display = "none";
+
 }
