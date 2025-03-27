581c05813ea9e69076c7e594af66c9fa248e6d0e
derick-rufino <derick10011@gmail.com>
2025-03-27 14:27:05 -0300

    Layout definido

---
+++ func-exe10.js
@@ -0,0 +1,23 @@
+//váriaveis globais
+const description = document.getElementById("desc-p")
+const imgSection = document.getElementById("img-holder")
+
+function addSin(){
+    description.innerHTML = "1"
+}
+
+function addTrans(){
+    description.innerHTML = "2"
+}
+
+function addDiag(){
+    description.innerHTML = "3"
+}
+
+function addTrat(){
+    description.innerHTML = "4"
+}
+
+function addPrev(){
+    description.innerHTML = "5"
+}
